e38b0b403256808e37d40c21a27c3855c7efc2b9
Sai Kommuru <saikommuru@Sais-Laptop.local>
2026-06-12 00:54:26 -0400

    Update stats and project details

--- components/data/stats.ts
+++ components/data/stats.ts
@@ -2,6 +2,6 @@ import { Stat } from "./types";
 
 export const stats: Stat[] = [
   { value: "91%", label: "ML model accuracy" },
-  { value: "2+", label: "Production apps shipped" },
+  { value: "4+", label: "Production apps shipped" },
   { value: "<24hr", label: "Response time" },
 ];
